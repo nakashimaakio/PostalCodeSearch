f2757000759d58445aeefcb3b19945a5f2ad2a7f
nakashima <nakashima@mobileit.co.jp>
2020-12-16 16:32:00 +0900

    ViewBinding対応

--- app/src/main/java/com/photograph/postalcodesearch/MainActivity.kt
+++ app/src/main/java/com/photograph/postalcodesearch/MainActivity.kt
@@ -1,39 +1,36 @@
 package com.photograph.postalcodesearch
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.eclipsesource.json.Json
+import com.photograph.postalcodesearch.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private val url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=1600000"
+    private val url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode="
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val getButton = findViewById<Button>(R.id.button)
-        getButton.setOnClickListener { onParallelGetButtonClick() }
+        binding.button.setOnClickListener { onParallelGetButtonClick() }
     }
 
-    //非同期処理でHTTP GETを実行
+    /** 非同期処理で住所検索 */
     private fun onParallelGetButtonClick() = GlobalScope.launch(Dispatchers.Main) {
         val http = HttpUtil()
-        //Mainスレッドでネットワーク関連処理を実行するとエラーになるためBackgroundで実行
-        withContext(Dispatchers.Default) { http.httpGet(url) }.let {
+        withContext(Dispatchers.Default) { http.httpGet(url + binding.postalCode.text) }.let {
             try {
-                //minimal-jsonを使って　jsonをパース
                 val result = Json.parse(it).asObject()
-                val textView = findViewById<TextView>(R.id.Prefecture)
-                textView.text =
+                binding.Prefecture.text =
                     result.get("results").asArray()[0].asObject().get("address1").asString()
             } catch (e: Exception) {
-
+                binding.Prefecture.text = "Error"
             }
         }
     }
