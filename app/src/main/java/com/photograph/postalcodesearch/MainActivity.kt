cc2df72384f43061b434886c09031c9cfdc569ca
nakashima <nakashima@mobileit.co.jp>
2020-12-16 14:49:22 +0900

    warning解消

--- app/src/main/java/com/photograph/postalcodesearch/MainActivity.kt
+++ app/src/main/java/com/photograph/postalcodesearch/MainActivity.kt
@@ -1,42 +1,35 @@
 package com.photograph.postalcodesearch
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.eclipsesource.json.Json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    val URL = "https://qiita.com/api/v2/items/bf3e4e06022eebe8e3eb" //サンプルとしてQiitaのAPIサービスを利用します
-    var result = ""
+    private val url = "https://qiita.com/api/v2/items/bf3e4e06022eebe8e3eb" //Qiita APIサービス
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val getButton = findViewById(R.id.button) as Button
-        getButton.setOnClickListener(object : View.OnClickListener {
-            override
-            fun onClick(view: View) {
-                onParallelGetButtonClick()
-            }
-        })
+        val getButton = findViewById<Button>(R.id.button)
+        getButton.setOnClickListener { onParallelGetButtonClick() }
     }
 
-    //非同期処理でHTTP GETを実行します。
-    fun onParallelGetButtonClick() = GlobalScope.launch(Dispatchers.Main) {
+    //非同期処理でHTTP GETを実行
+    private fun onParallelGetButtonClick() = GlobalScope.launch(Dispatchers.Main) {
         val http = HttpUtil()
         //Mainスレッドでネットワーク関連処理を実行するとエラーになるためBackgroundで実行
-        async(Dispatchers.Default) { http.httpGet(URL) }.await().let {
+        withContext(Dispatchers.Default) { http.httpGet(url) }.let {
             //minimal-jsonを使って　jsonをパース
             val result = Json.parse(it).asObject()
-            val textView = findViewById(R.id.Prefecture) as TextView
-            textView.setText(result.get("likes_count").asInt().toString() + "LGTM!")
+            val textView = findViewById<TextView>(R.id.Prefecture)
+            textView.text = result.get("likes_count").asInt().toString() + "LGTM!"
         }
     }
 }
